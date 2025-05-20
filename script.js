4c7d6062c8de386773152a672ff950abf35d0a7a
Dennis Limbu <141339233+dennislimbu@users.noreply.github.com>
2025-05-20 22:38:32 +0100

    fixing index.html layout, chaing gradient colour in mouse effect, adding contact page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var cursor = document.querySelector('.blob');
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
+if (cursor) {
+  document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
+  });
+}
